011cb8ff47149581d4104367a94f6c5a2bdb859f
Graywolf <akosheev@rambler.ru>
2020-08-30 14:18:06 +0300

    Refactoring forms test.

--- image_resize/tests/test_forms.py
+++ image_resize/tests/test_forms.py
@@ -1,21 +1,24 @@
+import pytest
 from image_resize.forms import ImageCreateForm, ResizeForm
 
 
-def test_image_create_url_field_label():
-    form = ImageCreateForm()
-    assert form.fields['image_url'].label == 'Ссылка'
+@pytest.mark.image_create_form
+class TestImageCreateForm:
+    def test_image_create_url_field_label(self):
+        form = ImageCreateForm()
+        assert form.fields['image_url'].label == 'Ссылка'
+
+    def test_image_create_file_field_label(self):
+        form = ImageCreateForm()
+        assert form.fields['image_file'].label == 'Файл'
 
 
-def test_image_create_file_field_label():
-    form = ImageCreateForm()
-    assert form.fields['image_file'].label == 'Файл'
+@pytest.mark.image_resize_form
+class TestImageResizeForm:
+    def test_resize_form_image_width_field_label(self):
+        form = ResizeForm()
+        assert form.fields['image_width'].label == 'Ширина'
 
-
-def test_resize_form_image_width_field_label():
-    form = ResizeForm()
-    assert form.fields['image_width'].label == 'Ширина'
-
-
-def test_resize_form_image_height_field_label():
-    form = ResizeForm()
-    assert form.fields['image_height'].label == 'Высота'
+    def test_resize_form_image_height_field_label(self):
+        form = ResizeForm()
+        assert form.fields['image_height'].label == 'Высота'
