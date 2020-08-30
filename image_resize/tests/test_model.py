c62d767fb56c76c3f189c003be9fc27da57ce994
Graywolf <akosheev@rambler.ru>
2020-08-30 22:20:19 +0300

    Add model test.

---
+++ image_resize/tests/test_model.py
@@ -0,0 +1,17 @@
+import pytest
+from image_resize.models import Image
+
+
+@pytest.mark.django_db
+def test_create_image():
+    image = Image.objects.create(
+        image_original='images/original/test.jpg',
+        image_name='images/test.jpg',
+        image_width=100,
+        image_height=100
+    )
+    assert image.filename() == 'test.jpg'
+    assert image.image_original == 'images/original/test.jpg'
+    assert image.image_name == 'images/test.jpg'
+    assert image.image_width == 100
+    assert image.image_height == 100
